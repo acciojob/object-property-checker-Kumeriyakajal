d05e07c3eda09364c379782efb7e3ad356448bf4
ketki kumeriya <111123802+Kumeriyakajal@users.noreply.github.com>
2023-07-02 16:07:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-
 function hasKey(key) {
+	
+	let val=Object.keys(sampleObject).includes(key)
+	
+return val;
+
   //   write your code here
 }
 
